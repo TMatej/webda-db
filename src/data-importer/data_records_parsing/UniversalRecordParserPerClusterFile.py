c02eceee2de259c73c6d43368adde135d12fed54
Matej Turek <matej.turek@pinya.cz>
2024-10-31 18:26:42 +0100

    feat: common functionality for UniversalRecordParsers was extracted to UniversalRecordParserBase

--- src/data-importer/data_records_parsing/UniversalRecordParserPerClusterFile.py
+++ src/data-importer/data_records_parsing/UniversalRecordParserPerClusterFile.py
@@ -1,107 +1,12 @@
 import os
 
-from common.constants import CLUSTERS_DATA_FOLDER_NAME, SQL_FILE_SUFFIX, DATA_TYPES_FILE_NAME
+from common.constants import CLUSTERS_DATA_FOLDER_NAME, SQL_FILE_SUFFIX
 from common.create_sql_insert_methods import write_sql_insert_statement, write_sql_values_keyword_statement
 from common.file_paths import DESTINATION_DATA_FOLDER_PATH, ORIGIN_CLUSTERS_FOLDER_PATH, \
-    ORIGIN_PUBLICATIONS_FOLDER_PATH, PROCESSED_DATA_FILES_PATH, NOT_PROCESSED_FILES_PATH
+    PROCESSED_DATA_FILES_PATH, NOT_PROCESSED_FILES_PATH
+from data_records_parsing.UniversalRecordParserBase import process_record, check_record_file_structure, \
+    process_data_types
 from data_type_parsing.DataType import DataType
-from data_type_parsing.DataTypesParser import extract_data_type
-
-# TODO - checking need refactor as the columns sometimes hold more information (more columns) than stated in the format
-def check_record_file_structure(open_file, data_type: DataType):
-    first_line = open_file.readline().strip()
-
-    stripped_first_line = first_line.lower()
-    cols = data_type.cols.replace("\\t", "\t").lower()
-
-    first_line_tuples = stripped_first_line.split("\t")
-    cols_tuples = cols.split("\t")
-
-    if len(first_line_tuples) > len(cols_tuples):
-        sanitized_cols = data_type.cols.replace("\t", "\\t")
-        sanitized_stripped_first_line = first_line.replace("\t", "\\t")
-        print(f'Header columns does not match the number of predefined columns. Expected: "{sanitized_cols}" - received: "{sanitized_stripped_first_line}". "{len(cols_tuples)}" - "{len(first_line_tuples)}".')
-        raise ValueError(f'Header columns does not match the number of predefined columns. Expected: "{sanitized_cols}" - received: "{sanitized_stripped_first_line}". "{len(cols_tuples)}" - "{len(first_line_tuples)}".')
-
-
-    for i in range(len(first_line_tuples)):
-            first_line_part = first_line_tuples[i].strip()
-            cols_part = cols_tuples[i].strip()
-
-            if first_line_part != cols_part:
-                sanitized_cols = data_type.cols.replace("\t", "\\t")
-                sanitized_stripped_first_line = first_line.replace("\t", "\\t")
-                print(f'Expected: "{sanitized_cols}" - received: "{sanitized_stripped_first_line}". "{cols_part}" - "{first_line_part}".')
-                raise ValueError(f'Expected: "{sanitized_cols}" - received: "{sanitized_stripped_first_line}". "{cols_part}" - "{first_line_part}".')
-
-    # if stripped_first_line != cols:
-    #     sanitized_cols = cols.replace("\t", "\\t")
-    #     sanitized_stripped_first_line = stripped_first_line.replace("\t", "\\t")
-    #     print(f'First line does not match the columns. Expected: "{sanitized_cols}" - received: "{sanitized_stripped_first_line}".')
-    #     raise ValueError(f'First line does not match the columns. Expected: "{sanitized_cols}" - received: "{sanitized_stripped_first_line}".')
-
-    # this can be optional
-    # second_line = open_file.readline().lower()
-    # stripped_second_line = second_line.rstrip().lower()
-    # under = data_type.under.replace("\\t", "\t")
-    # if stripped_second_line != under:
-    #     sanitized_under = under.replace("\t", "\\t")
-    #     sanitized_stripped_second_line = stripped_second_line.replace("\t", "\\t")
-    #     print(f'Second line does not match the format. Expected: "{sanitized_under}" - received: "{sanitized_stripped_second_line}".')
-    #     raise ValueError(f'Second line does not match the format. Expected: "{sanitized_under}" - received: "{sanitized_stripped_second_line}".')
-
-    # reset pointer to beginning
-    open_file.seek(0)
-
-def process_record(folder_name, line: str, column_formats: [str]) -> str | None:
-    line_tuple = line.strip("\n").split("\t")
-
-    if len(line_tuple) > len(column_formats):
-        column_formats_sanitized_string = "\\t".join(column_formats)
-        line_tuple_sanitized_string = "\\t".join(line_tuple)
-        print(f'Number of values does not match the number of predefined columns. Expected: "{column_formats_sanitized_string}" - received: "{line_tuple_sanitized_string}". "{len(column_formats)}" - "{len(line_tuple)}".')
-        raise ValueError(f'Number of values does not match the number of predefined columns. Expected: "{column_formats_sanitized_string}" - received: "{line_tuple_sanitized_string}". "{len(column_formats)}" - "{len(line_tuple)}".')
-
-    line = f"'{folder_name}'"
-
-    for i in range(len(column_formats)):
-        line = "".join([line, ", "])
-
-        if i >= len(line_tuple):
-            line = "".join([line, "NULL"])
-            continue
-
-        column_format = column_formats[i]
-        column_value = line_tuple[i].strip()
-
-        # empty value -> NULL
-        if len(column_value) == 0 :
-            line = "".join([line, "NULL"])
-            continue
-
-        # number or string value
-        if "d" in column_format or "f" in column_format:
-            line = "".join([line, column_value])
-        else:
-            line = "".join([line, "'", column_value, "'"])
-
-    return line
-
-def process_data_types() -> [DataType]:
-    source_file = os.path.join(ORIGIN_PUBLICATIONS_FOLDER_PATH, DATA_TYPES_FILE_NAME)
-
-    data_types: [DataType] = []
-
-    with open(source_file, "rt") as source:
-        while True:
-            data_type = extract_data_type(source)
-
-            if data_type is None:
-                break
-
-            data_types.append(data_type)
-
-    return data_types
 
 
 def process_records_of_data_type(
@@ -211,20 +116,17 @@ def main():
     # process data types
     data_types = process_data_types()
 
-    # make processed data file map
-
     processed_data_types_names = []
     with open(PROCESSED_DATA_FILES_PATH, "rt") as processed_data_types_file:
         processed_data_types_names = list(map(str.strip, processed_data_types_file.readlines()))
 
     for data_type in data_types:
         # DATA TYPE METADATA PROCESSING
-        file_name = data_type.file_name.strip().lower()
+        data_type_file_name = data_type.file_name.strip().lower()
 
-        if file_name in processed_data_types_names:
+        if data_type_file_name in processed_data_types_names:
             continue
 
-        # compute the values for each data type once
         data_type_table_name = data_type.file_name.strip().lower().replace(".", "_").capitalize() # TODO - make mapper for types
         columns: [str] = data_type.cols.strip().split("\\t")
         columns.insert(0, "foldername")
@@ -243,14 +145,14 @@ def main():
                 data_type,
                 folder_name,
                 cluster_sql_folder_path,
-                file_name,
+                data_type_file_name,
                 data_type_table_name,
                 data_type_table_parameters,
                 columns_formats)
 
-        processed_data_types_names.append(file_name)
+        processed_data_types_names.append(data_type_file_name)
         with open(PROCESSED_DATA_FILES_PATH, "at") as processed_data_types_file:
-            processed_data_types_file.write(file_name + "\n")
+            processed_data_types_file.write(data_type_file_name + "\n")
 
 if __name__ == '__main__':
     main()
