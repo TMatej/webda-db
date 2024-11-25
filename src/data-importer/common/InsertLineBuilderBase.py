2dfdb327178ace6a370b30a2ef9cb8ac9fba504d
Matej Turek <matej.turek@pinya.cz>
2024-11-25 22:16:17 +0100

    feat: static methods of InsertLineBuilderBase, __sanitize_string_value__() and __sanitize_numeric_value__() were provided with types for input parameters and __sanitize_numeric_value__() was extended to contain simple check of values for correct decimal parsing.

--- src/data-importer/common/InsertLineBuilderBase.py
+++ src/data-importer/common/InsertLineBuilderBase.py
@@ -1,11 +1,33 @@
+from decimal import Decimal, InvalidOperation
+
+
 class InsertLineBuilderBase:
     @staticmethod
-    def __sanitize_string_value__(value):
+    def __sanitize_string_value__(value: str):
         return "NULL" if not value else "'" + value.replace("'", "''") + "'"
 
     @staticmethod
-    def __sanitize_numeric_value__(value):
-        return "NULL" if not value else value
+    def __sanitize_numeric_value__(value: str):
+        if not value:
+            return "NULL"
+
+        while value.__contains__("--"):
+            value = value.replace("--", "-")
+
+        try:
+            num = str(Decimal(value))
+        except InvalidOperation:
+            print(f"Invalid input: '{value}' cannot convert to integer.")
+
+            # some values have random "/  " with their value
+            value = value.strip("/").strip()
+            try:
+                num = str(Decimal(value))
+            except InvalidOperation:
+                print(f"Invalid input: '{value}' cannot convert to integer.")
+                num = "NULL"
+
+        return num
 
     def build_insert_values_line(self) -> str:
         return f"()"
